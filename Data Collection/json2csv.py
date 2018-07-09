530bac7e2e501c31c09343fabb68cdc9b8a3fba9
Muhammad Daniyal Dar <ddar203@gmail.com>
2018-07-09 09:50:56 +0500

    Crawler Fixed & Results CSV Parsed

--- Data Collection/json2csv.py
+++ Data Collection/json2csv.py
@@ -1,16 +1,31 @@
 import json
 import csv
 
-x = json.load(open('crawlerResults.json'))
-
+x = json.load(open('crawlerResult.json'))
 f = csv.writer(open("test.csv", "wb+"))
 
-# Write CSV Header, If you dont need that, remove this line
-
-f.writerow([y for y in x])
+f.writerow(
+			['videoId',
+			'title',
+			'channelId',
+			'viewCount',
+			'commentCount',
+			'comments',
+			'tags',
+			'description',
+		])
 
 for i in range(len(x['videoId'])):
 	try:
-		f.writerow([x[y][i] for y in x])
-	except:
-		continue
+		f.writerow(
+			['https://www.youtube.com/watch?v='+x['videoId'][i],
+			x['title'][i].encode('ascii','ignore'),
+			x['channelId'][i],
+			x['viewCount'][i],
+			x['commentCount'][i],
+			x['comments'][i],
+			x['tags'][i],
+			x['description'][i].encode('ascii','ignore'),
+		])
+	except Exception,e:
+		print str(e)
