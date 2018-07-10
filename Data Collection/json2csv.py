9ce6440a9e4d48b75491ffbce379f183ddbe83eb
Rashna <19100079@lums.edu.pk>
2018-07-10 16:17:09 +0500

    updated csv with links,category and classification

--- Data Collection/json2csv.py
+++ Data Collection/json2csv.py
@@ -1,18 +1,25 @@
 import json
 import csv
 
-x = json.load(open('crawlerResult.json'))
+x = json.load(open('csvdata.json'))
 f = csv.writer(open("test.csv", "wb+"))
 
 f.writerow(
 			['videoId',
 			'title',
 			'channelId',
+			'category',
+			'classification',
 			'viewCount',
 			'commentCount',
 			'comments',
 			'tags',
 			'description',
+			'links',
+			'linksCount',
+			'linksDownCount',
+			'linksDown',
+			'linksUp'
 		])
 
 for i in range(len(x['videoId'])):
@@ -21,11 +28,18 @@ for i in range(len(x['videoId'])):
 			['https://www.youtube.com/watch?v='+x['videoId'][i],
 			x['title'][i].encode('ascii','ignore'),
 			x['channelId'][i],
+			x['category'][i],
+			x['classification'][i],
 			x['viewCount'][i],
 			x['commentCount'][i],
 			x['comments'][i],
 			x['tags'][i],
 			x['description'][i].encode('ascii','ignore'),
+			x['links'][i],
+			x['linksCount'][i],
+			x['linksDownCount'][i],
+			x['linksDown'][i],
+			x['linksUp'][i]
 		])
 	except Exception,e:
 		print str(e)
