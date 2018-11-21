8558ccd5e75a69defbda8e894b3cc01653729bc4
rashnakumar96 <31850566+rashnakumar96@users.noreply.github.com>
2018-11-21 21:02:18 +0500

    Add files via upload

--- Complete Analysis/domainChannelsVideos.py
+++ Complete Analysis/domainChannelsVideos.py
@@ -6,15 +6,19 @@ import urllib2
 import sys
 import threading
 import time
+from apiclient.discovery import build
+
+DEVELOPER_KEY = 'AIzaSyClserQ1cuNOX9SssQT6-BvBf65JZZ1Lk4'
+youtube = build('youtube', 'v3', developerKey=DEVELOPER_KEY)
 
 channels = json.load(open('domainChannels.json'))
 
 chan = {}
 count=0
 for channel in channels.keys():
-	# count+=1
-	# if count>=2:
-	# 	break
+	count+=1
+	if count>=5:
+		break
 	print "channel"
 	chanVids = json.loads(urllib2.urlopen("https://www.googleapis.com/youtube/v3/search?key=AIzaSyClserQ1cuNOX9SssQT6-BvBf65JZZ1Lk4&channelId="+channel+"&part=snippet,id&order=date&maxResults=50").read())
 	# d = []
@@ -32,29 +36,32 @@ for channel in channels.keys():
 	comments = []
 	tags = []
 	description = []
-	chan['channel']={}
+	chan[channel]={}
 	for k in range(len(chanVids['items'])):
 		if 'videoId' in chanVids['items'][k]['id']:
 			videoId.append('https://www.youtube.com/watch?v='+chanVids['items'][k]['id']['videoId'])
-			title.append(chanVids['items'][k]['snippet']['title']) 
+			results = youtube.videos().list(id=chanVids['items'][k]['id']['videoId'], part='snippet').execute()
+			for result in results.get('items', []):
+
+				title.append(result['snippet']['title']) 
+				try:
+					categoryId.append(result['snippet']['categoryId'])
+				except:
+					categoryId.append('No categoryId')
+				try:
+					viewCount.append(result['statistics']['viewCount'])
+				except:
+					viewCount.append('No viewCount')
+				try:
+					tags.append(result['snippet']['tags'])
+				except:
+					tags.append('No tags')
+				try:
+					description.append(result['snippet']['description'])
+				except:
+					description.append('No description')
 			try:
-				categoryId.append(chanVids['items'][k]['snippet']['categoryId'])
-			except:
-				categoryId.append('No categoryId')
-			try:
-				viewCount.append(chanVids['items'][k]['statistics']['viewCount'])
-			except:
-				viewCount.append('No viewCount')
-			try:
-				tags.append(chanVids['items'][k]['snippet']['tags'])
-			except:
-				tags.append('No tags')
-			try:
-				description.append(chanVids['items'][k]['snippet']['description'])
-			except:
-				description.append('No description')
-			try:
-				commentsOnVideo = json.loads(urllib2.urlopen("https://www.googleapis.com/youtube/v3/commentThreads?key=AIzaSyB84_YL94d4I_7ABN0ZCxnX10DxOQzAV74&textFormat=plainText&part=snippet&videoId=" + chanVids['items'][k]['id']['videoId'] + "&maxResults=50",timeout=60).read())
+				commentsOnVideo = json.loads(urllib2.urlopen("https://www.googleapis.com/youtube/v3/commentThreads?key=AIzaSyB84_YL94d4I_7ABN0ZCxnX10DxOQzAV74&textFormat=plainText&part=snippet&videoId=" + chanVids['items'][k]['id']['videoId'],timeout=60).read())
 				c = []
 				for k in range(len(commentsOnVideo['items'])):
 					c.append(commentsOnVideo['items'][k]["snippet"]['topLevelComment']['snippet']['textDisplay'])
@@ -69,19 +76,19 @@ for channel in channels.keys():
 			except:
 			    commentCount.append("No commentCount")
 
-	chan['channel']['title']=title
-	chan['channel']['categoryId']=categoryId
-	chan['channel']['videoId']=videoId
-	chan['channel']['viewCount']=videoId
-	chan['channel']['commentCount']=commentCount
-	chan['channel']['comments']=comments
-	chan['channel']['tags']=tags
-	chan['channel']['description']=description
+	chan[channel]['title']=title
+	chan[channel]['categoryId']=categoryId
+	chan[channel]['videoId']=videoId
+	chan[channel]['viewCount']=viewCount
+	chan[channel]['commentCount']=commentCount
+	chan[channel]['comments']=comments
+	chan[channel]['tags']=tags
+	chan[channel]['description']=description
 
 
 	# break
-# print chan
-with open('domainChannelsVideos.json','w') as f:
+print chan.keys()
+with open('domainChannelsVids.json','w') as f:
 	json.dump(chan,f)		        
 # if response['items'][0]['snippet']['channelId'] not in chan:
 #     chan[response['items'][0]['snippet']['channelId']] = d
